388ef2b94b2acb5870b20262aca8c307a53539b4
siskafks <120354576+siskafks@users.noreply.github.com>
2023-06-16 07:58:35 +0700

    Update router-predict.js

--- CC/routes/router-predict.js
+++ CC/routes/router-predict.js
@@ -17,18 +17,32 @@ router.get('/:kabupaten/:kecamatan', async (req, res) => {
     const kecamatan = req.params.kecamatan.toLowerCase();
 
     try {
-      const urls = [];
-      for (let i = 1; i <= 10; i++) {
-        const imageUrl = `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/${i}.png`;
-        const [fileExists] = await storage.bucket(bucketName).file(`${kabupaten}/${kecamatan}/${i}.png`).exists();
+      const prediction = {
+        temperature_2m: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/1.png`,
+        relativehumidity_2m: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/2.png`,
+        apparent_temperature: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/3.png`,
+        precipitation: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/4.png`,
+        rain: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/5.png`,
+        cloudcover: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/6.png`,
+        shortwave_radiation: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/7.png`,
+        direct_radiation: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/8.png`,
+        diffuse_radiation: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/9.png`,
+        direct_normal_irradiance: `https://storage.googleapis.com/${bucketName}/${kabupaten}/${kecamatan}/10.png`
+      };
 
+      for (let i = 1; i <= 10; i++) {
+        const [fileExists] = await storage.bucket(bucketName).file(`${kabupaten}/${kecamatan}/${i}.png`).exists();
         if (!fileExists) {
           throw new Error('File not found');
         }
-          urls.push(imageUrl);
-        }
-
-        res.json(urls);
+      }
+     
+      const response = {
+        message: 'success',
+        prediction
+      };
+  
+      res.json(response);
 
     } catch (err) {
       console.error(err);
@@ -36,4 +50,4 @@ router.get('/:kabupaten/:kecamatan', async (req, res) => {
     }
   });
   
-  module.exports = router;
+module.exports = router;
